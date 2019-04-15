f9b0ee68b47ba3e1807e93435f75569d1d8f3c18
Lynda <lynda0214@gmail.com>
2019-04-15 16:21:30 +0800

    English segments by spaces, adding ellipsis to the tail, and fix 7th line is showed in picture issue

--- server.js
+++ server.js
@@ -18,21 +18,28 @@ app.get('/', function (req, res) {
 })
 
 app.listen(process.env.PORT || 5000, function () { // for heroku tcp 80 is acceptable
-  // console.log('tsaibrobetalnuts app listening on port 5000!')
+  console.log('tsaibrobetalnuts app is listening!')
 })
 
 app.post('/', function(req, res) {
     let origin = req.body.origin;
     
-    origin = origin.replace(/\s+/g, ""); // remove blanks
+    // origin = origin.replace(/\s+/g, ""); // remove blanks
+    origin = origin.trim(); // remove both sides blanks 
 
     let reg = new RegExp(/[.,:;?!。，：；、？！]/g); // remove non words
     origin = origin.replace(reg, ''); // 
-    console.log(origin);
+    // console.log(origin);
 
     node_jieba_parsing([dict1, dict2], origin, function (result) {
-        console.log(result.join(" "));
-        res.json({result: result.join("..."), error: null})
+        
+        result = result.filter((word)=>(word!==' '));
+        let result2str = result.join("...");
+        result2str = result2str + "...";
+
+        console.log(result2str);
+        
+        res.json({result: result2str, error: null})
         //res.render('index', {result: result.join("..."), error: null});
     });
 
